837036fabb179614e51eba87a53565813203c228
Patrik Machala <machalp@u2-14.ms.mff.cuni.cz>
2019-03-26 16:29:23 +0100

    Add Kelvin conversion

--- src/Main.java
+++ src/Main.java
@@ -25,22 +25,31 @@ public class Main {
     }
     public static void showCtoF(){
         JFrame frame = new JFrame("prevodnik");
-        frame.setLayout(new GridLayout(2,2));
+        frame.setLayout(new GridLayout(3,3));
         JTextField field = new JTextField();
         JLabel celsium = new JLabel("°C");
         JButton convert = new JButton("Convert");
         JLabel fahren = new JLabel(" F");
+        JButton convertKelvin = new JButton("Convert to K");
+        JLabel Kelvin = new JLabel(" K");
         convert.addActionListener(e -> fahren.setText(Convert(field.getText())));
+        convertKelvin.addActionListener(e -> fahren.setText(ConvertToK(field.getText())));
         frame.add(field);
         frame.add(celsium);
         frame.add(convert);
         frame.add(fahren);
+        frame.add(convertKelvin);
+        frame.add(Kelvin);
         frame.setSize(200,200);
         frame.setVisible(true);
     }
     public static String Convert(String c){
         int celsium = Integer.parseInt(c);
         return (celsium*18)+32+" F";
+    }
+    public static String ConvertToK(String c){
+        int celsium = Integer.parseInt(c);
+        return (celsium+273.15)+" F";
     }
     public static void main(String[] args) {
         //javax.swing.SwingUtilities.invokeLater(() -> showHello());
